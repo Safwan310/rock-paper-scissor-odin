cea2de470a9b076497df3fdc5bc252c14411e1a2
Mohammed Safwan <mdsafwan3102001@gmail.com>
2021-01-29 00:44:03 +0530

    Made some changes to the headings with DOM Manipulation

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let playerscore = document.querySelector('#youscore')
 let pcscore = document.querySelector('#pcscore')
 let container = document.querySelector('.playbuttons')
 let reset = document.querySelector('#reset')
+let sb = document.querySelector('.points')
 let plsc = 0
 let pcsc = 0
 
@@ -27,7 +28,7 @@ buttons.forEach((button)=>{
     function playRound(ps,cs){
         ps = ps.toLowerCase()
         cs = cs.toLowerCase()
-        
+
         if(ps==='rock'&&cs==='paper'||ps==='paper'&&cs==='scissors'||ps==='scissors'&&cs==='rock'){
             announcer.textContent = 'You lose '+cs+' defeats '+ps
             pcsc += 1
@@ -51,31 +52,20 @@ buttons.forEach((button)=>{
                 announcer.textContent = 'DEFEAT! You have lost to your PC'
                 pcscore.textContent = ''
                 playerscore.textContent = ''
+                sb.innerText = ''
                 pcsc +=1000
             }
             else{
                 announcer.textContent = 'VICTORY! You have defeated your PC'
                 pcscore.textContent = ''
                 playerscore.textContent = ''
+                sb.innerText = ''
                 plsc +=1000
             }
-            
+
         }
         return pcsc,plsc
     }
 }
 
 game()
-
-
-
-
-
-
-
-
-
-
-
-
-
