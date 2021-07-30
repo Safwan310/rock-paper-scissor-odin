76a949f142f987c0ef08d912d3f55ae99dd0702f
Mohammed Safwan <mdsafwan3102001@gmail.com>
2021-07-30 14:28:08 +0530

    Changed the game logic

--- script.js
+++ script.js
@@ -1,71 +1,55 @@
-const buttons = Array.from(document.querySelectorAll('#play'))
-let playerscore = document.querySelector('#youscore')
-let pcscore = document.querySelector('#pcscore')
-let container = document.querySelector('.playbuttons')
-let reset = document.querySelector('#reset')
-let sb = document.querySelector('.points')
-let plsc = 0
-let pcsc = 0
-
-function csselect(){
-    let gameArray = ['Rock', 'Paper','Scissors']
-    return  gameArray[Math.floor(Math.random()*gameArray.length)]
-}
-reset.addEventListener('click',()=>window.location.reload())
-
-let announcer = document.querySelector('#announcer')
-
-
-function game(){
-    let playerSelection
-buttons.forEach((button)=>{
-    button.addEventListener('click',()=>{
-        playerSelection = button.innerText
-        let computerSelection = csselect()
-        playRound(playerSelection,computerSelection)
-    })
-})
-    function playRound(ps,cs){
-        ps = ps.toLowerCase()
-        cs = cs.toLowerCase()
-
-        if(ps==='rock'&&cs==='paper'||ps==='paper'&&cs==='scissors'||ps==='scissors'&&cs==='rock'){
-            announcer.textContent = 'You lose '+cs+' defeats '+ps
-            pcsc += 1
-            pcscore.textContent = 'PC: '+pcsc
-            playerscore.textContent = 'You: '+plsc
-            console.log(pcsc)
-        }
-        else if(ps===cs){
-            announcer.textContent = 'It\'s a tie'
-            console.log(pcsc)
-        }
-        else{
-            announcer.textContent = 'You win '+ps+' defeats '+cs
-            plsc += 1
-            pcscore.textContent = 'PC: '+pcsc
-            playerscore.textContent = 'You: '+plsc
-            console.log(pcsc)
-        }
-        if(pcsc>=5||plsc>=5){
-            if(pcsc>plsc){
-                announcer.textContent = 'DEFEAT! You have lost to your PC'
-                pcscore.textContent = ''
-                playerscore.textContent = ''
-                sb.innerText = ''
-                pcsc +=1000
+let playerScore = 0,computerScore = 0;
+let announcer = document.querySelector("#announcer");
+function game(playerSelection){
+    if(playerScore < 5 && computerScore < 5)
+    {let computerSelection = csselect();
+        console.log(playerSelection+"=="+computerSelection);
+    if((playerSelection == "rock"&&computerSelection == "paper") ||
+       (playerSelection == "paper" && computerSelection == "scissors") ||
+       (playerSelection == "scissors" && computerSelection == "rock"))
+       {
+            computerScore += 1;
+            document.querySelector("#cDisplay").innerText = computerScore;
+            if(computerScore == 5){
+                announcer.innerText = "GAME OVER! You lose. Click restart to start a new game";
             }
             else{
-                announcer.textContent = 'VICTORY! You have defeated your PC'
-                pcscore.textContent = ''
-                playerscore.textContent = ''
-                sb.innerText = ''
-                plsc +=1000
+                announcer.innerText = `Point for computer! ${computerSelection} beats ${playerSelection}`;
             }
-
+            
+       }
+    else if(playerSelection == computerSelection){
+        announcer.innerText = "Tie, Points for none";
+    }
+    else{
+        playerScore += 1;
+        document.querySelector("#playerDisplay").innerText = playerScore;
+        if(playerScore == 5){
+            announcer.innerText = "GAME OVER! You win.  Click restart to start a new game";
         }
-        return pcsc,plsc
+        else{
+            announcer.innerText = `Point for you! ${playerSelection} beats ${computerSelection}`;
+        }
+    }
+    }
+    else{
+        if(playerScore > computerScore){
+            announcer.innerText = "GAME OVER! You win.  Click restart to start a new game";
+        }
+        else if(playerScore == computerScore){
+            announcer.innerText = "GAME OVER! It's a tie. Click restart to start a new game"
+        }
+        else{
+            announcer.innerText = "GAME OVER! You lose. Click restart to start a new game"
+        }
     }
 }
 
-game()
+function csselect(){
+    let gameArray = ['rock', 'paper','scissors']
+    return  gameArray[Math.floor(Math.random()*gameArray.length)]
+}
+
+let reset = document.querySelector('#reset');
+
+reset.addEventListener('click',()=>window.location.reload())
